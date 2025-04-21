60c61f896bbb4103ddeeb26e694a19998264545c
dolaniyanu <danielolaniyanu@gmail.com>
2025-04-21 01:13:53 -0500

    [1.02] db_operations - create weather table

--- db_operations.py
+++ db_operations.py
@@ -4,4 +4,24 @@
 ###  CLASS : ADEV-3005 (261248)
 ###  DATE  : 2025-04-10
 
+from dbcm import DBCM
+
+DB_NAME = "weather.sqlite"
+
+class DBOperations:
+
+
+    def initialize_db():
+
+        # Create Weather Table using context manager derived cursor
+        with DBCM(DB_NAME) as cursor:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS weather 
+                                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                 date TEXT NOT NULL,
+                                 location TEXT NOT NULL,
+                                 min_temp REAL,
+                                 max_temp REAL,
+                                 avg_temp REAL);
+                           """)
+
 
