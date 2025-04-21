82efba7c94fd5fae93115a8f7d25356a09f4018f
dolaniyanu <danielolaniyanu@gmail.com>
2025-04-21 00:54:50 -0500

    1.01 - DBCM class

---
+++ dbcm.py
@@ -0,0 +1,28 @@
+### PROJECT
+### dbcm.py
+###  NAME  : DANIEL OLANIYANU
+###  CLASS : ADEV-3005 (261248)
+###  DATE  : 2025-04-20
+
+import sqlite3
+
+class DBCM:
+
+    def  __init__(self, db_name):
+        self.db_name = db_name
+        self.connection = None
+        self.cursor = None
+
+    def  __enter__(self):
+        self.connection = sqlite3.connect(self.db_name) # Establish SQLite database connection.
+        self.cursor = self.connection.cursor() # Make connection an extension of cursor definition.
+        return self.cursor # Return cursor
+    
+    def __exit__(self, error_check):
+        if self.connection: # Checks if there is a database connection.
+            if error_check is None:
+                self.connection.commit() # Saves Inserts if there is no error.
+            else:
+                self.connection.rollback() # Undo changes if there is an error.
+            self.cursor.close() 
+            self.connection.close()
